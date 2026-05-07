320c4d2efbaa7c46a6224e1e5af9a9e62cf0ffb3
Syed Meesam Raza Rizvi <syedmeesamrazarizvi@MacBook-Air-2.local>
2026-05-07 14:25:38 +0200

    refactor: add remote state + improve CI pipeline structure

--- main.tf
+++ main.tf
@@ -1,13 +1,11 @@
 terraform {
-
   backend "s3" {
-    bucket         = "terraform-state-web-app-bucket-2.0" # must be globally unique
+    bucket         = "terraform-state-web-app-bucket-2.0"
     key            = "web-app/terraform.tfstate"
     region         = "us-east-1"
-    encrypt        = true
     dynamodb_table = "terraform-state-locking"
+    encrypt        = true
   }
-
 
   required_providers {
     aws = {
@@ -15,43 +13,10 @@ terraform {
       version = "6.39.0"
     }
   }
-
-
 }
 
 provider "aws" {
   region = "us-east-1"
-}
-
-resource "aws_s3_bucket" "terraform_state" {
-  bucket        = "terraform-state-web-app-bucket-2.0" # REPLACE WITH YOUR BUCKET NAME
-  force_destroy = true
-}
-
-resource "aws_s3_bucket_versioning" "terraform_bucket_versioning" {
-  bucket = aws_s3_bucket.terraform_state.id
-  versioning_configuration {
-    status = "Enabled"
-  }
-}
-
-resource "aws_s3_bucket_server_side_encryption_configuration" "terraform_state_crypto_conf" {
-  bucket = aws_s3_bucket.terraform_state.bucket
-  rule {
-    apply_server_side_encryption_by_default {
-      sse_algorithm = "AES256"
-    }
-  }
-}
-
-resource "aws_dynamodb_table" "terraform_locks" {
-  name         = "terraform-state-locking"
-  billing_mode = "PAY_PER_REQUEST"
-  hash_key     = "LockID"
-  attribute {
-    name = "LockID"
-    type = "S"
-  }
 }
 
 resource "aws_instance" "instance-1" {
